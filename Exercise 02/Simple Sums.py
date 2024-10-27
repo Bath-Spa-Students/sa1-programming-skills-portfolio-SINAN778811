0163fdcf5b2aa237d835270c113c05c7e588d26e
SINAN778811 <sinanmedattil77@gmail.com>
2024-10-27 15:41:15 +0400

    Simple Sums.py

---
+++ Exercise 02/Simple Sums.py
@@ -0,0 +1,9 @@
+# Exercise no 2
+# Assing a variable and initialze it with the integer value 8
+num1=8
+# Assing a second variable and initialze it with the integer value 10
+num2=10
+# Assing a third variable that stores the sum of first two numbers 
+sum=num1+num2
+# print value of sum to console.
+print("the total sum is: ",sum)
