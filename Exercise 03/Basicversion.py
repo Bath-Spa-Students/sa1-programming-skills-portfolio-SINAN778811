8ec073cdbf4a581d47b32a828923fd6edd0c3b31
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-11 01:40:13 +0400

    Advancedversion.py

---
+++ Exercise 03/Basicversion.py
@@ -0,0 +1,11 @@
+# Step 1: Reserve the information in a dictionary
+person_info = {
+    "name": "Sinan",
+    "hometown": "Kerala",
+    "age": 18
+}
+
+# Step 2: Publish the values on separate lines using a single print statement
+print(f"Name: {person_info['name']}")
+print(f"Hometown: {person_info['hometown']}")
+print(f"Age: {person_info['age']}")
