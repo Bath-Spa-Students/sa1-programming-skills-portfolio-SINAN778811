5b37d87fa3d7f219ca4dab77f06794c34dcb0c58
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-11 01:42:05 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello")  # This line imprints "Hello" to the console
+
+def main():
+    hello()  # This line reaches out the hello() function
+
+if __name__:
+    main()  # This shouts the main() function when the script is run
