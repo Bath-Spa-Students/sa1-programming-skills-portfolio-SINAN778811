ae0ca93d3c905c201131de820064a00bd4a617fc
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-10 23:43:17 +0400

    simplesearch.py

---
+++ Exercise 08/simplesearch.py
@@ -0,0 +1,11 @@
+# Checking the list of names
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+# Adding the name to search for
+name_to_find = "Sam"
+
+# Check if the name is in the list
+if name_to_find in names:
+    print(f"{name_to_find} found in the list!")
+else:
+    print(f"{name_to_find} not found in the list.")
