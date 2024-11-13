11ff98624a32afc22c20c4b9f74a158ec17ab8d5
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-13 14:50:02 +0400

    Delete Exercise 04/Primitive Quiz standard.py

--- Exercise 04/Primitive Quiz standard.py
+++
@@ -1,8 +0,0 @@
-# Exercise no 4, Primitive Quiz standard 
-# Asking the capital of france 
-Capital = input("what is the capital of france: ")
-# Checking the capital correct or incorrect 
-if Capital == "Paris":
-    print("the answer is correct")
-else : 
-    print("the answer is incorrect")
