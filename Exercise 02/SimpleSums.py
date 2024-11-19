c83df5794d9651ac1a8f6fde662d8ac260566c34
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-19 16:20:11 +0400

    Delete SimpleSums.py

--- Exercise 02/SimpleSums.py
+++
@@ -1,9 +0,0 @@
-# Exercise no 2
-# Assign a variable and initialze it with the integer value 8
-num1=8
-# Assign a second variable and initialze it with the integer value 10
-num2=10
-# Assign a third variable that stores the sum of first two numbers 
-sum=num1+num2
-# print value of sum to console.
-print("the total sum is: ",sum)
