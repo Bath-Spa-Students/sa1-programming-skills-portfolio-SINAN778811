544a74a528198cb428623a60f15b51d863fafa28
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 09:49:42 +0400

    Variables (Favorite Number).py

---
+++ z_bonus_exercise/Variables (Favorite Number).py
@@ -0,0 +1,8 @@
+# Signify your favorite digit using a variable
+favorite_digit = 4
+
+# generate a message revealing your favorite digit
+message = f"My favorite digit is {favorite_digit}."
+
+# Print the message
+print(message)
