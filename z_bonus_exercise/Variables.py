09e9e4263f3d7cfbb8da37faca15e7e79b062d75
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 09:48:58 +0400

    Variables.py

---
+++ z_bonus_exercise/Variables.py
@@ -0,0 +1,7 @@
+# Allocate a message to a variable
+message = "I am sinan."
+print(message)
+
+# Replace the value of the variable to a new message
+message = "I am eighteen years old."
+print(message)
