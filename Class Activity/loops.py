d3a5fd293938d6068110db8be84dc19c19963c2d
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 13:21:09 +0400

    loops.py

---
+++ Class Activity/loops.py
@@ -0,0 +1,21 @@
+s= 1
+while s < 10:
+  print(s)
+  s+= 1 
+
+
+  c= 1
+while c <= 5:
+    print("Count is:", c)
+    c += 1
+
+    r = 1
+while r < 6:
+  print(r)
+  if r == 3:
+    break
+  r += 1   # r=r+1  Both are same 
+
+fruits = ["apple", "banana", "cherry"]
+for i in fruits:
+  print(i)
