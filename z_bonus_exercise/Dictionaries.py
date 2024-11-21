d340ab3b8379134640d9abbcb4270501f0b5fb76
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 09:52:14 +0400

    Dictionaries.py

---
+++ z_bonus_exercise/Dictionaries.py
@@ -0,0 +1,13 @@
+# Accumlate figures about a person in a dictionary
+person = {
+    "first_name": "Muhammed",
+    "last_name": "Sinan",
+    "age": 18,
+    "city": "Emiratescity"
+}
+
+# Print each piece of figures saved in the dictionary
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
