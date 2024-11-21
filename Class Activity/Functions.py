ff3a384b115817f99129932e5ec10457b3f3c605
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 13:26:48 +0400

    Functions.py

---
+++ Class Activity/Functions.py
@@ -0,0 +1,12 @@
+# Void function 
+def print_message():
+    print("Hello Students")
+# Calling function 
+print_message()
+
+def print_message():
+    message ="Hello Students"  # Local variable 
+    print(message)
+    
+# message is not defined bcz local varaible life is inside of program 
+print_message()
