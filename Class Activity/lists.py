45517b2335a10d397a43f871a8ec61cf317bb07a
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 13:20:13 +0400

    lists.py

---
+++ Class Activity/lists.py
@@ -0,0 +1,24 @@
+# Printing the list 
+names =["Sinan" , "Bathspa" , "Code Lab 1" ]
+print(names)
+
+numbers = [1,2,3,4]
+new_numbers = numbers * 2
+print(new_numbers)
+
+# Positive Indexing  
+numbers =[5,6,7,8,3,4]
+print(numbers[3]) 
+
+# Negative indexing starts from -1  , bcz 0 is a non-negative integer 
+numbers =[5,6,7,6,3,4]
+print(numbers[-3]) 
+
+# Mutability
+numbers =[5,6,7,8,3,4]
+numbers[0]=1
+
+# Removing item from list 
+New_list7 = [4,7,6,3,1]
+New_list7.remove(6)
+print(New_list7)
