1a97ad88c4ff9269afe8ae86a6542a0af44aa0f6
SINAN778811 <sinanmedattil77@gmail.com>
2024-11-21 09:50:32 +0400

    List -Names.py

---
+++ z_bonus_exercise/List -Names.py
@@ -0,0 +1,8 @@
+# Retain names of friends in a list
+names = ["Sinan", "Anaswara", "Shazin", "Amaan"]
+
+# publish each person's name by accessing each element
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
